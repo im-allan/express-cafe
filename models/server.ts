cee77f707dc9997470af27b29b124cb1b5ba162a
im-allan <al.silvaquiroz@gmail.com>
2023-10-30 17:34:17 -0300

    feat: Added server and user model

---
+++ models/server.ts
@@ -0,0 +1,52 @@
+import  express, {Express} from 'express';
+import { dbConnection } from '../database/config.db';
+const cors = require('cors')
+class Server {
+  
+  app: Express;
+  port: string | undefined;
+  usersPath: string;
+
+  constructor () {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usersPath = '/api/users';
+
+    // Conectar a bd
+    this.connectDB();
+
+    // Middlewares
+    this.middlewares();
+    
+    // Rutas de mi aplicación
+    this.routes();
+  }
+
+  async connectDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors())
+
+    // Lectura del body
+    this.app.use(express.json());
+    
+    // Directorio público
+    this.app.use(express.static('../public'));
+  }
+
+  routes() {
+    this.app.use( this.usersPath, require('../routes/user.routes'));
+  }
+  
+  listen(){
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}!\n`) 
+    })
+  }
+
+}
+
+export default Server;
---
+++ models/user.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'El nombre es obligatorio']
+  },
+  email: {
+    type: String,
+    required: [true, 'El correo es obligatorio'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria'],
+  },
+  img: {
+    type: String,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['ADMIN_ROLE', 'USER_ROLE']
+  },
+  status: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  },
+})
+
+export default model('User', UserSchema);
