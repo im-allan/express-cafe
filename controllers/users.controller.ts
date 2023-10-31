ee3e4453d5c5d0e482388216455fe1fc146719f9
im-allan <al.silvaquiroz@gmail.com>
2023-10-31 17:49:54 -0300

    feat: Added CRUD for users

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,37 +1,58 @@
 import { Response, Request } from "express";
+import bcrypt from "bcrypt";
+
 import User from '../models/user';
-export const usersGet = (req: Request, res: Response) => {
-  
+
+
+export const usersGet = async(req: Request, res: Response) => {
+  const { limit = 5, skip = 0 } = req.query;
+
+  const [total, users] = await Promise.all([
+    User.countDocuments({ status: true }),
+    User.find({ status: true })
+      .skip(Number(skip))
+      .limit(Number(limit))
+  ]);
+
   res.json({
-    msg: 'GET API - Controller',
+    total,
+    users
   })
 }
 
 export const usersPost = async(req: Request, res:  Response) => {
-  const body = req.body;
-  const user = new User(body);
+  const { name, email, password, role } = req.body;
+  const user = new User({name, email, password, role});
+
+  // Encriptar password
+  const salt = bcrypt.genSaltSync();
+  user.password = bcrypt.hashSync( password, salt );
+  
+  // Guardar en BD
   await user.save();
   res.status(201).json({
     user
   })
 }
 
-export const usersPut = (req: Request, res: Response) => {
+export const usersPut = async(req: Request, res: Response) => {
   const { id } = req.params;
-  res.status(200).json({
-    msg: 'PUT API - Controller',
-    id
-  })
+  const { _id, password, google, email, ...rest } = req.body;
+
+  if (password) {
+    const salt = bcrypt.genSaltSync();
+    rest.password = bcrypt.hashSync( password, salt );
+  }
+
+  const user = await User.findByIdAndUpdate(id, rest)
+ 
+  res.status(200).json(user)
 }
 
-export const usersPatch = (req: Request, res: Response) => {
-  res.status(200).json({
-    msg: 'PATCH API - Controller',
-  })
-}
+export const usersDelete = async(req: Request, res: Response) => {
+  const { id } = req.params;
 
-export const usersDelete = (req: Request, res: Response) => {
-  res.json({
-    msg: 'DELETE API - Controller',
-  })
+  const user = await User.findByIdAndUpdate(id, {status: false});
+
+  res.json(user)
 }
