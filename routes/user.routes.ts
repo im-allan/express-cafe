8fd20f009a90719e4b1a95cf1c63ee0df2f17e97
im-allan <al.silvaquiroz@gmail.com>
2023-10-31 17:52:46 -0300

    feat: Added middlewares

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { usersGet, usersPost, usersPut, usersPatch, usersDelete } from '../controllers/users.controller';
+import { check } from 'express-validator';
+
+import { validateFields } from '../middlewares/validate-fields';
+import { existEmail, existUserById, isValidRole } from '../helpers/db-validator';
+
+import { usersGet, usersPost, usersPut, usersDelete } from '../controllers/users.controller';
 
 const router = Router();
 
@@ -7,10 +12,24 @@ module.exports = router;
 
 router.get('/', usersGet)
 
-router.post('/', usersPost)
+router.put('/:id', [
+  check('id', 'No es un ID válido').isMongoId(),
+  check('id').custom(existUserById),
+  check('role').custom(isValidRole),
+  validateFields
+], usersPut)
 
-router.put('/:id', usersPut)
+router.post('/', [
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('password', 'La contraseña debe tener más de 6 caracteres').isLength({ min: 6}),
+  check('email', 'El correo ingresado no es válido').isEmail(),
+  check('email',).custom(existEmail),
+  check('role').custom(isValidRole),
+  validateFields
+], usersPost)
 
-router.patch('/', usersPatch)
-
-router.delete('/', usersDelete)
+router.delete('/:id', [
+  check('id', 'No es un ID válido').isMongoId(),
+  check('id').custom(existUserById),
+  validateFields
+], usersDelete)
