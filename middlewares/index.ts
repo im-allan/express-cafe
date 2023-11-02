5781c7a7a51436482c2002bd3f5298c59a42f2ca
im-allan <al.silvaquiroz@gmail.com>
2023-11-01 21:37:42 -0300

    feat: Added barrel file

---
+++ middlewares/index.ts
@@ -0,0 +1,5 @@
+import { validateFields } from './validate-fields';
+import { validateJWT } from './validate-jwt';
+import { isAdminRole } from './validate-role';
+
+export { validateFields, validateJWT, isAdminRole };
