db48ab157f631d44266080e5cbe150adf55e67e3
im-allan <al.silvaquiroz@gmail.com>
2023-11-08 14:34:52 -0300

    feat: Added existParam function to validate if param exist

--- helpers/db-validator.ts
+++ helpers/db-validator.ts
@@ -1,5 +1,4 @@
-import Role from '../models/role'; 
-import User from '../models/user';
+import { Category, Product, Role, User } from '../models';
 
 export const isValidRole = async(role:string)=> {
   const existRole = await Role.findOne({ role });
@@ -19,5 +18,32 @@ export const existUserById = async(id: string) => {
   const existUser = await User.findById(id);
   if (!existUser) {
     throw new Error(`ERROR: No existe un usuario con el id ${id}`);
+  }
+}
+
+export const existCategoryById = async(id: string) => {
+  const existCategory = await Category.findById(id);
+  if (!existCategory) {
+    throw new Error(`ERROR: No existe una categoría con el id ${id}`);
+  }
+}
+
+export const existProductById = async(id: string) => {
+  const existProduct = await Product.findById(id);
+  if (!existProduct) {
+    throw new Error(`ERROR: No existe un producto con el id ${id}`);
+  }
+}
+
+export const existParam = (param: string | number) => {
+
+  if (typeof param === 'string' && param.length === 0) {
+    throw new Error('El parámetro no debe estar vacío');
+
+  } else if (typeof param === 'number' && !Number.isInteger(param)) {
+    throw new Error('El número debe ser un entero');
+
+  } else {
+    return param;
   }
 }
